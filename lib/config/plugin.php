27da6133f1b71837e4c13e6befa9a093380edc9d
Maxim <62299292+mjyarush@users.noreply.github.com>
2023-03-06 13:21:44 +0300

    Change version on conf

--- lib/config/plugin.php
+++ lib/config/plugin.php
@@ -4,7 +4,7 @@ return array(
     'name'        => _wp('Reading time'),
     'description' => _wp('Adds an article reading time indicator'),
     'vendor'      => '667213',
-    'version'     => '2023.03.01',
+    'version'     => '2023.02.02',
     'img'         => 'img/book.png',
     'frontend'    => true,
     'handlers'  => array(
